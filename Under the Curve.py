9e552638a9141900174b2003e88c06bee9d9d3ea
giggletech <37095041+lizard-brain@users.noreply.github.com>
2023-08-25 18:16:46 +1000

    Addded Graph

--- Under the Curve.py
+++ Under the Curve.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
 from scipy import integrate
+from datetime import datetime
+import matplotlib.pyplot as plt
 
 # Read the data
 data = pd.read_csv('data.csv', delimiter=',')
-
 
 # Define the metric columns
 metric_columns = ['Delta_TP9', 'Delta_AF7', 'Delta_AF8', 'Delta_TP10',
@@ -13,39 +14,35 @@ metric_columns = ['Delta_TP9', 'Delta_AF7', 'Delta_AF8', 'Delta_TP10',
                   'Beta_TP9', 'Beta_AF7', 'Beta_AF8', 'Beta_TP10',
                   'Gamma_TP9', 'Gamma_AF7', 'Gamma_AF8', 'Gamma_TP10']
 
-
-
 # Remove rows with missing values in any of the metric columns
 data = data.dropna(subset=metric_columns)
 
+# Convert 'TimeStamp' column to datetime
+data['TimeStamp'] = pd.to_datetime(data['TimeStamp'])
 
-
-'''
-# Remove rows with extreme values (considering a simple threshold for this example)
-threshold = 10  # Adjust this threshold as needed
-data = data[(data.iloc[:, 1:] < threshold).all(axis=1)]
-'''
-
-
-# Save the cleaned data to a new CSV file
-cleaned_file_path = 'cleaned_data.csv'
-data.to_csv(cleaned_file_path, index=False)
-print(f"Cleaned data saved to '{cleaned_file_path}'")
-
+# Convert 'TimeStamp' to seconds or milliseconds since a reference point
+reference_time = data['TimeStamp'].min()  # Choose a reference time
+data['TimeInSeconds'] = (data['TimeStamp'] - reference_time).dt.total_seconds()
 
 # Calculate AUC for each metric
 results = {}
 for metric in metric_columns:
     try:
-        auc = integrate.simps(data[metric], data['TimeStamp'])
+        auc = integrate.simps(data[metric], data['TimeInSeconds'])
         results[metric] = auc
     except KeyError as e:
         print(f"Error: {e} not found in the data.")
 
-# Print the AUC results
-for metric, auc in results.items():
-    print(f'AUC for {metric}: {auc}')
+# Create a bar chart for AUC results
+plt.figure(figsize=(10, 6))
+plt.barh(list(results.keys()), list(results.values()), color='skyblue')
+plt.xlabel('Area Under the Curve (AUC)')
+plt.ylabel('Metrics')
+plt.title('AUC for EEG Metrics')
+plt.tight_layout()
 
-# Optionally, you can save the results to a CSV file
-results_df = pd.DataFrame(results, index=[0])
-results_df.to_csv('auc_results.csv', index=False)
+# Save the bar chart as an image
+plt.savefig('auc_bar_chart.png')
+
+# Display the bar chart
+plt.show()
