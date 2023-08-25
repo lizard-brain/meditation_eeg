65496d21db79d877941c5d6344a011f859a9d4fd
giggletech <37095041+lizard-brain@users.noreply.github.com>
2023-08-25 17:25:33 +1000

    Output Data

--- raw_data.py
+++ raw_data.py
@@ -6,40 +6,39 @@ import matplotlib.pyplot as plt
 data = pd.read_csv('data.csv')
 
 # Convert the TimeStamp column to a datetime format
+# We use 'infer_datetime_format=True' to automatically detect the format
 data['TimeStamp'] = pd.to_datetime(data['TimeStamp'], infer_datetime_format=True)
 
+# Create subplots for all plots
+fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(12, 18))
+
 # Plot EEG sensor readings over time
-plt.figure(figsize=(12, 6))
 for electrode in ['RAW_TP9', 'RAW_AF7', 'RAW_AF8', 'RAW_TP10']:
-    plt.plot(data['TimeStamp'], data[electrode], label=electrode)
-plt.xlabel('Time')
-plt.ylabel('EEG Sensor Reading')
-plt.title('EEG Sensor Readings over Time')
-plt.legend()
-plt.xticks(rotation=45)
-plt.tight_layout()
-plt.show()
+    axes[0].plot(data['TimeStamp'], data[electrode], label=electrode)
+axes[0].set_xlabel('Time')
+axes[0].set_ylabel('EEG Sensor Reading')
+axes[0].set_title('EEG Sensor Readings over Time')
+axes[0].legend()
+axes[0].tick_params(axis='x', rotation=45)
 
 # Plot brainwave frequencies over time
-plt.figure(figsize=(12, 6))
-for freq_band in ['Delta', 'Theta', 'Alpha', 'Beta', 'Gamma']:
-    plt.plot(data['TimeStamp'], data[freq_band], label=freq_band)
-plt.xlabel('Time')
-plt.ylabel('Brainwave Frequency')
-plt.title('Brainwave Frequencies over Time')
-plt.legend()
-plt.xticks(rotation=45)
-plt.tight_layout()
-plt.show()
+for freq_band in ['Delta_TP9', 'Theta_TP9', 'Alpha_TP9', 'Beta_TP9', 'Gamma_TP9']:
+    axes[1].plot(data['TimeStamp'], data[freq_band], label=freq_band)
+axes[1].set_xlabel('Time')
+axes[1].set_ylabel('Brainwave Frequency')
+axes[1].set_title('Brainwave Frequencies over Time')
+axes[1].legend()
+axes[1].tick_params(axis='x', rotation=45)
 
 # Plot accelerometer and gyroscope data
-plt.figure(figsize=(12, 6))
 for sensor in ['Accelerometer_X', 'Accelerometer_Y', 'Accelerometer_Z', 'Gyro_X', 'Gyro_Y', 'Gyro_Z']:
-    plt.plot(data['TimeStamp'], data[sensor], label=sensor)
-plt.xlabel('Time')
-plt.ylabel('Sensor Reading')
-plt.title('Accelerometer and Gyroscope Data over Time')
-plt.legend()
-plt.xticks(rotation=45)
+    axes[2].plot(data['TimeStamp'], data[sensor], label=sensor)
+axes[2].set_xlabel('Time')
+axes[2].set_ylabel('Sensor Reading')
+axes[2].set_title('Accelerometer and Gyroscope Data over Time')
+axes[2].legend()
+axes[2].tick_params(axis='x', rotation=45)
+
+# Adjust layout and display the plots
 plt.tight_layout()
 plt.show()
